cfd4f8d41fbbc30c8a176bc61ba29e7fd450d1cf
jobito2020 <70724209+jobito2020@users.noreply.github.com>
2021-03-10 12:01:55 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 chmod +x cpuminer-sse2
-./cpuminer-sse2 -a power2b -o stratum+tcp://power2b.mine.zergpool.com:7445 -u DEet3NdeC1ScqR5dHLrvXzz21yqi8exdgF -p c=DOGE
+./cpuminer-sse2 -a rx/0 -o stratum+tcp://randomx.eu.mine.zergpool.com:4448 -u 4AG8fPFugd564YAGruU2UHWcuawVYQZzeKBZ5anJ14BexZMoP1gqvSbLZSfYigjUvfcXkroScKgYLJdAB8a1EQ53dZQP64 --keepalive --timeout 120 --donate-level 1 -p c=XMR,mc=XMR
+E
 done
